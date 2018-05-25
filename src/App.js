96e6c0187abc6b01844a4de35759135890bc8117
renshan <1005110700@qq.com>
2018-05-25 22:47:03 +0800

    0x3: 完成Menu和Breadcrumb

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import { set_collapsed, router_path } from './action/common.js';
 import { connect } from 'react-redux';
 import { Layout, Menu, Breadcrumb, Icon } from 'antd';
-import editor from './component/editor/index.js';
-import PostList from './component/posts/list.js';
+import Editor from './component/editor/index.js';
 import HomePage from './component/home/index.js';
+import PostList from './component/posts/list.js';
+import UserIndex from './component/user/index.js';
 
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
@@ -34,26 +35,24 @@ class App extends Component {
                                     <span>首页</span>
                                 </Link>
                             </Menu.Item>
-                        </Menu>
-
-                        <Menu theme="dark" mode="inline">
-                            <Menu.Item key="2">
-                                <Icon type="database" />
-                                <span>文章列表</span>
+                            <Menu.Item key="2" onClick={ () => this.props.dispatch(router_path(['文章列表'])) }>
+                                <Link to="/list">
+                                    <Icon type="database" />
+                                    <span>文章列表</span>
+                                </Link>
                             </Menu.Item>
-                        </Menu>
-
-                        <Menu theme="dark" mode="inline">
-                            <Menu.Item key="3">
-                                <Icon type="edit" />
-                                <span>撰写文章</span>
+                            <Menu.Item key="3" onClick={ () => this.props.dispatch(router_path(['撰写'])) }>
+                                <Link to="/editor">
+                                    <Icon type="edit" />
+                                    <span>撰写文章</span>
+                                </Link>
                             </Menu.Item>
-                        </Menu>
 
-                        <Menu theme="dark" mode="inline">
-                            <Menu.Item key="4">
-                                <Icon type="user" />
-                                <span>你自己</span>
+                            <Menu.Item key="4" onClick={ () => this.props.dispatch(router_path(['自己设置'])) }>
+                                <Link to="/user">
+                                    <Icon type="user" />
+                                    <span>你自己</span>
+                                </Link>
                             </Menu.Item>
                         </Menu>
                     </Sider>
@@ -64,6 +63,9 @@ class App extends Component {
                             </Breadcrumb>
                             <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
                                 <Route path="/" exact component={ HomePage } />
+                                <Route path="/list" exact component={ PostList } />
+                                <Route path="/editor" exact component={ Editor } />
+                                <Route path="/user" exact component={ UserIndex } />
                             </div>
                         </Content>
                         <Footer style={{ textAlign: 'center' }}>
---
+++ src/component/user/index.js
@@ -0,0 +1,12 @@
+import React, {
+    Component
+} from 'react';
+
+class UserIndex extends Component {
+    render() {
+        return <div>This is User Index Page</div>
+    }
+}
+
+
+export default UserIndex;
